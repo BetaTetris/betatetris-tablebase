5749422f4b444f9c9b6eff7e923cc130e809921b
adrien1018 <adrien1018@users.noreply.github.com>
2025-07-02 11:55:30 -0700

    Update crash dodge lua

--- lua/Tetris.lua
+++ lua/Tetris.lua
@@ -1,10 +1,9 @@
-currentSeedAddress = 0x0037
-
 gameStateAddress = 0x00C0
 playStateAddress = 0x0048
 isLevelEnterAddress = 0x0764
-currentLevelAddress = 0x0765
-nowTetriminoIDAddress = 0x0042
+selectLevelAddress = 0x0047
+copyrightAddress1 = 0x00C3
+nowTetriminoIDAddress = 0x0062
 nextTetriminoIDAddress = 0x00BF
 
 tetriminoXAddress = 0x0041
@@ -109,16 +108,18 @@ end
 
 function receiveSequence(tcp, seq)
   if not seq.length then
-    local p = tryReceive(tcp, 2)
+    local p = tryReceive(tcp, 3)
     if p then
+      --printBytes(p)
       if string.byte(p, 1) == 0xfe then
-        seq.length = string.byte(p, 2)
+        seq.length = string.byte(p, 2) * 256 + string.byte(p, 3)
       end
     end
   end
   if seq.length and not seq[seq.length] then
     local p = tryReceive(tcp, seq.length)
     if p then
+      --printBytes(p)
       for i = 1,seq.length do
         local x = string.byte(p, i)
         local buttons = {}
@@ -126,6 +127,8 @@ function receiveSequence(tcp, seq)
         if x % 4 >= 2 then buttons.right = true end
         if x % 8 >= 4 then buttons.A = true end
         if x % 16 >= 8 then buttons.B = true end
+        if x % 32 >= 16 then buttons.down = true end
+        if x % 64 >= 32 then buttons.select = true end
         seq[i] = buttons
       end
       return true
@@ -172,12 +175,19 @@ function getLines()
 end
 
 function getScore()
-  local r1 = memory.readwordunsigned(0x0008)
-  local r2 = memory.readwordunsigned(0x000A)
-  return r2 * 65536 + r1
+  local r1 = memory.readbyteunsigned(0x0055)
+  local r2 = memory.readbyteunsigned(0x0054)
+  local r3 = memory.readbyteunsigned(0x0053)
+  local score = math.floor(r1 / 16)
+  score = score * 10 + r1 % 16
+  score = score * 10 + math.floor(r2 / 16)
+  score = score * 10 + r2 % 16
+  score = score * 10 + math.floor(r3 / 16)
+  score = score * 10 + r3 % 16
+  return score
 end
 
-function gameLoop(tcp, level, seed, terminate)
+function gameLoop(tcp, level, terminate)
   resetQueue(tcp)
   sendStartGame(tcp, level)
   local endGame = false
@@ -212,6 +222,9 @@ function gameLoop(tcp, level, seed, terminate)
           end
         end
       end
+      if nFrame > 1000 then
+        return
+      end
       emu.frameadvance()
       st = memory.readbyteunsigned(playStateAddress)
       nFrame = nFrame + 1
@@ -221,30 +234,44 @@ function gameLoop(tcp, level, seed, terminate)
     local rotate = rotateMap[memory.readbyteunsigned(tetriminoRotateAddress)]
     local x = memory.readbyteunsigned(tetriminoXAddress)
     local y = memory.readbyteunsigned(tetriminoYAddress)
-    if false then -- early terminate
+    local score = getScore()
+    if false and score < 1300000 and score < 6857 * getLines() - 137143 then
       while st ~= 10 do
         emu.frameadvance()
         st = memory.readbyteunsigned(playStateAddress)
       end
     end
+    local pcnt = 0
+    local clearframe = 0
     while st ~= 1 do
       if st == 10 then
         endGame = true
+        io.write(tostring(emu.framecount()) ..  ' ' .. tostring(emu.lagcount()) .. ' ' .. tostring(pcnt) .. ' ' .. tostring(getScore()) .. ' ' .. tostring(getLines()) .. '\n')
+        io.flush()
         print(getScore(), getLines())
         break
       end
+      if st == 6 and clearframe == 0 then
+        clearframe = emu.framecount()
+      end
+      if (pcnt == 40 and st == 6 and memory.readbyteunsigned(0x00B1) ~= 255) or pcnt == 60 then
+        joypad.set(1, {select=true})
+      end
+      if pcnt > 1000 then
+        return
+      end
       emu.frameadvance()
       st = memory.readbyteunsigned(playStateAddress)
+      pcnt = pcnt + 1
     end
+    io.write(tostring(clearframe) .. ' ' .. tostring(emu.framecount()) ..  ' ' .. tostring(emu.lagcount()) .. ' ' .. tostring(pcnt) .. ' ' .. tostring(getScore()) .. ' ' .. tostring(getLines()) .. '\n')
+    io.flush()
     receiveTwoSequence(tcp, curSequence, fNextSequence, nFrame, 2)
     nextSequence = fNextSequence
     local nextPiece = memory.readbyteunsigned(nextTetriminoIDAddress)
     trySend(tcp, string.char(0xfd, rotate, x, y, pieceMap[nextPiece]))
   end
   if not terminate then return end
-  for i = 1,60 do
-    emu.frameadvance()
-  end
   while memory.readbyteunsigned(gameStateAddress) == 4 do
     joypad.set(1, {start=true})
     for i = 1,5 do
@@ -255,140 +282,78 @@ end
 
 --- menu
 
+function enterFromMode()
+  for i = 1,3 do
+    emu.frameadvance()
+  end
+  for i = 1,3 do
+    joypad.set(1, {down=true})
+    emu.frameadvance()
+    emu.frameadvance()
+  end
+  joypad.set(1, {start=true})
+  emu.frameadvance()
+end
+
+function enterFromMain()
+  for i = 1,4 do
+    emu.frameadvance()
+  end
+  joypad.set(1, {start=true})
+  emu.frameadvance()
+  enterFromMode()
+end
+
 function waitStart()
-  for i = 1,14 do
+  while memory.readbyteunsigned(gameStateAddress) == 0 do
     emu.frameadvance()
-  end
-end
-
-function enableDoubleKs()
-  for i = 1,4 do
-    emu.frameadvance()
-    joypad.set(1, {up=true})
-    emu.frameadvance()
-  end
-  joypad.set(1, {right=true})
-  emu.frameadvance()
-  for i = 1,4 do
-    emu.frameadvance()
-    joypad.set(1, {down=true})
-    emu.frameadvance()
-  end
-end
-
-function moveToSeed()
-  for i = 1,2 do
-    emu.frameadvance()
-    joypad.set(1, {down=true})
-    emu.frameadvance()
-  end
-end
-
-function randomSeed()
-  joypad.set(1, {select=true})
-  emu.frameadvance()
-end
-
-function hexToNum(chr)
-  local ascii = string.byte(chr)
-  if ascii >= 48 and ascii <= 57 then
-    return ascii - 48
-  end
-  if ascii >= 65 and ascii <= 70 then
-    return ascii - 65 + 10
-  end
-  if ascii >= 97 and ascii <= 102 then
-    return ascii - 97 + 10
-  end
-  return 0
-end
-
-function currentSeed(digit)
-  local addr = currentSeedAddress + math.floor((digit - 1) / 2)
-  local val = memory.readbyteunsigned(addr)
-  if digit % 2 == 0 then
-    return val % 16
-  else
-    return math.floor(val / 16)
-  end
-end
-
-function inputSeed(seed)
-  for i = 1,6 do
-    joypad.set(1, {right=true})
-    emu.frameadvance()
-    emu.frameadvance()
-    emu.frameadvance()
-    local digit = hexToNum(string.sub(seed, i, i))
-    while true do
-      local cur = currentSeed(i)
-      if cur == digit then
-        break
-      end
-      if (cur + 16 - digit) % 16 < 8 then
-        joypad.set(1, {down=true})
-      else
-        joypad.set(1, {up=true})
-      end
-      emu.frameadvance()
-      emu.frameadvance()
+    if memory.readbyteunsigned(copyrightAddress1) == 0 then
+      joypad.set(1, {start=true})
     end
   end
-end
-
-function enterMenu()
-  joypad.set(1, {start=true})
-  for i = 1,5 do
-    emu.frameadvance()
-  end
+  enterFromMain()
 end
 
 function startGame(level)
-  while memory.readbyteunsigned(isLevelEnterAddress) ~= 1 do
-    joypad.set(1, {right=true})
+  for i = 1,4 do
+    emu.frameadvance()
+  end
+  local x = level % 10
+  while true do
+    local now = memory.readbyteunsigned(selectLevelAddress)
+    if now == x then
+      break
+    elseif now < 5 then
+      joypad.set(1, {down=true})
+    elseif now < x then
+      joypad.set(1, {right=true})
+    else
+      joypad.set(1, {left=true})
+    end
     emu.frameadvance()
     emu.frameadvance()
   end
-  while memory.readbyteunsigned(currentLevelAddress) < level do
-    joypad.set(1, {up=true})
+  if level >= 10 then
+    joypad.set(1, {A=true})
     emu.frameadvance()
+    joypad.set(1, {A=true, start=true})
+    emu.frameadvance()
+  else
+    emu.frameadvance()
+    joypad.set(1, {start=true})
     emu.frameadvance()
   end
-  while memory.readbyteunsigned(currentLevelAddress) > level do
-    joypad.set(1, {down=true})
-    emu.frameadvance()
+  while memory.readbyteunsigned(playStateAddress) ~= 1 do
     emu.frameadvance()
   end
-  joypad.set(1, {start=true})
-  emu.frameadvance()
-  for i = 1,8 do
+  for i = 1,3 do -- fix occassional lag
     emu.frameadvance()
   end
 end
 
-function backToMain()
-  emu.frameadvance()
-  joypad.set(1, {B=true})
-  for i = 1,10 do
-    emu.frameadvance()
-  end
-  joypad.set(1, {right=true})
-  for i = 1,2 do
-    emu.frameadvance()
-  end
-end
+for z = 3,3 do
 
-function generateRandomSeed()
-  local s = ''
-  for i = 1,6 do
-    local num = math.random(1, 16)
-    s = s .. string.sub('0123456789abcdef', num, num)
-  end
-  print(s)
-  return s
-end
-
-math.randomseed(123)
+math.randomseed(13)
 math.random()
 local socket = require("socket")
 local tcp = assert(socket.tcp())
@@ -399,20 +364,31 @@ if not ret then
 end
 tcp:settimeout(0.001, 't')
 
-if true then
-  emu.poweron()
-  waitStart()
-  enableDoubleKs()
-  moveToSeed()
+--io.output(io.open('crash-dodge.log', 'a'))
+io.write('start ' .. tostring(z) .. '\n')
+
+emu.poweron()
+--emu.speedmode("turbo")
+--emu.speedmode("maximum")
+waitStart()
+
+for i = 1,z do
+  emu.frameadvance()
 end
 
+startGame(18)
+gameLoop(tcp, 18)
+
+for i = 1,10 do
+  emu.frameadvance()
+end
+
+tcp:close()
+
+end
+
+emu.pause()
+
 while true do
-  local seed = generateRandomSeed()
-
-  inputSeed(seed)
-  enterMenu()
-
-  startGame(18)
-  gameLoop(tcp, 18, seed, true)
-  backToMain()
+  emu.frameadvance()
 end
